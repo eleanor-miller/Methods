211843e696f0c3c48e51806cc65e6e83ed93a16e
Eleanor Miller <eleanormiller.dev@gmail.com>
2021-08-23 15:08:09 -0400

    Prompt for String and Integer

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ Input     None
 Work      Print greeting to the console
 Output    None
 
-method type   output type   inputs
+method type   output type   method name / input
 static        void          DisplayGreeting()
 */
 
@@ -22,20 +22,52 @@ namespace EmployeeDatabase
       Console.WriteLine();
       Console.WriteLine();
     }
+
+    static string PromptForString(string prompt)
+    {
+      Console.WriteLine("🐙🐙🐙");
+      // Use the argument, whatever the caller sent us.
+      Console.Write(prompt);
+
+      // Get some user input
+      var userInput = Console.ReadLine();
+
+      // RETURN that value as the output of this method. 
+      // The value in 'userInput' will go wherever the 
+      // *CALLER* of the method has specified.
+      return userInput;
+    }
+
+    static int PromptForInteger(string prompt)
+    {
+      var userInput = PromptForString(prompt);
+
+      int inputAsInteger;
+      var isThisGoodInput = int.TryParse(userInput, out inputAsInteger);
+
+      if (isThisGoodInput)
+      {
+        return inputAsInteger;
+      }
+      else
+      {
+        Console.WriteLine("That isn't a valid number. Please try again.");
+        return 0;
+      }
+
+    }
+
     static void Main(string[] args)
     {
       DisplayGreeting();
 
-      Console.Write("What is your name? ");
-      var name = Console.ReadLine();
+      var name = PromptForString("What is your name? ");
 
-      Console.Write("What is your department number? ");
-      var department = int.Parse(Console.ReadLine());
+      var department = PromptForInteger("What is your department number? ");
 
-      Console.Write("What is your yearly salary (in dollars)? ");
-      var salary = int.Parse(Console.ReadLine());
-      
-      var salaryPerMonth = salary / 12;
+      var salary = PromptForInteger("What is your yearly salary (in dollars)? ");
+
+      var salaryPerMonth = salary / 12.0;
       Console.WriteLine($"Hello, {name} you make {salaryPerMonth} a month.");
     }
 } }
